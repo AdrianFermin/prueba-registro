f8caa5ad90cc911981bc5c54e9c4565d2729f12c
AdrianFermin <adrianferminmartinez@gmail.com>
2022-11-08 10:43:37 -0400

    Updated BranchPrueba

--- registrar.php
+++ registrar.php
@@ -5,11 +5,11 @@ include("con_db.php");
 if (isset($_POST['register'])) {
 
     if (strlen($_POST['nombre']) >= 1 && strlen($_POST['email']) >= 1) {
-        $name = trim($_POST['nombre']);
+        $name = trim($_POST['name']);
         $email = trim($_POST['email']);
         $fechareg = date("d/m/y");
-        $consulta = "INSERT INTO datos(nombre, email, fecha_reg) VALUES ('$name','$email','$fechareg')";
-        $resultado = mysqli_query($conex, $consulta);
+        $consult = "INSERT INTO datos(nombre, email, fecha_reg) VALUES ('$name','$email','$fechareg')";
+        $resultado = mysqli_query($conexion, $consult);
 
         if ($resultado) {
 ?>
@@ -17,12 +17,12 @@ if (isset($_POST['register'])) {
         <?php
         } else {
         ?>
-            <h3 class="bad">Ups ha ocurrido un error</h3>
+            <h3 class="mal">Ups ha ocurrido un error</h3>
         <?php
         }
     } else {
         ?>
-        <h3 class="bad">Complete los datos</h3>
+        <h3 class="mal">Complete los datos</h3>
 <?php
     }
 }
