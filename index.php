90d01658f8924b19441652f53fc9297ee76fdfac
AdrianFermin <adrianferminmartinez@gmail.com>
2022-11-08 10:21:51 -0400

    Updated

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="estilos.css">
+    <title>Formulario PhP</title>
+</head>
+
+<body>
+    <form method="POST">
+        <h1>Registrar Usuario</h1>
+        <input type="text" name="nombre" placeholder="Nombre Completo">
+        <input type="email" name="email" placeholder="Email">
+        <input type="submit" name="register" value="Registrar">
+    </form>
+
+    <?php
+    include("registrar.php");
+    ?>
+
+</body>
+
+</html>
